f23b6ab187e7722c3c21bbaa56d148b7786ab973
ftvalex01 <ftvalex@hotmail.com>
2023-09-23 22:36:28 +0100

    Ejercicio 2 - A5 Completo

--- forocars/app/Providers/RouteServiceProvider.php
+++ forocars/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/community';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
--- forocars/database/seeders/DatabaseSeeder.php
+++ forocars/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\CommunityLink;
+use DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
+        // \App\Models\User::factory(10)->create(); 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::delete('delete from community_links');
+        CommunityLink::factory()->count(50)->create();
     }
 }
