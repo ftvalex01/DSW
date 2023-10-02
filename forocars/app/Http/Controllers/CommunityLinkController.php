0b4f6406dc837bab31e48899d68a9eb2d4540a32
ftvalex01 <ftvalex@hotmail.com>
2023-10-02 21:31:29 +0100

    A8 Ejercicio 1. Añadir canales terminado

--- forocars/app/Http/Controllers/CommunityLinkController.php
+++ forocars/app/Http/Controllers/CommunityLinkController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Channel;
 use App\Models\CommunityLink;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommunityLinkController extends Controller
 {
@@ -13,7 +14,9 @@ class CommunityLinkController extends Controller
     public function index()
     {
         $links = CommunityLink::paginate(25);
-        return view('community/index', compact('links'));
+        $channels = Channel::orderBy('title','asc')->get();
+        
+        return view('community/index', compact('links','channels'));
     }
 
     /**
@@ -31,13 +34,12 @@ class CommunityLinkController extends Controller
     {
         $data = $request->validate([
             'title' => 'required|max:255',
-
+            'channel_id' => 'required|exists:channels,id',
             'link' => 'required|unique:community_links|url|max:255',
         ]);
 
         $data['user_id'] = Auth::id();
 
-        $data['channel_id'] = 1;
 
         CommunityLink::create($data);
 
--- forocars/database/migrations/2023_09_19_170956_create_channels_table.php
+++ forocars/database/migrations/2023_09_19_170956_create_channels_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('channels', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('color');
             $table->timestamps();
         });
     }
