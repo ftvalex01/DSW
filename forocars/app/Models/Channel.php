2527edd97af86f45942890253bd7c1bf81bae6ed
ftvalex01 <ftvalex@hotmail.com>
2023-10-02 22:02:37 +0100

    ejercicio 3 A8 TERMINADO

--- forocars/app/Models/Channel.php
+++ forocars/app/Models/Channel.php
@@ -13,4 +13,5 @@ class Channel extends Model
         'slug',
         'color'
     ];
+   
 }
--- forocars/resources/views/community/links.blade.php
+++ forocars/resources/views/community/links.blade.php
@@ -1,8 +1,14 @@
 @foreach ($links as $link)
+
 <li>
     <a href="{{ $link->link }}" target="_blank">
         {{ $link->title }}
     </a>
+    <span class="label label-default" style="background: {{ $link->channel->color }}">
+        {{ $link->channel->title }}
+        
+        </span>
     <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}</small>
 </li>
+
 @endforeach
