43289fdb454c41502b9af69a5e98bb1ec37f8a56
ftvalex01 <ftvalex@hotmail.com>
2023-10-20 21:41:37 +0100

    intentando que algo funcione

--- forocars/app/Models/CommunityLink.php
+++ forocars/app/Models/CommunityLink.php
@@ -24,13 +24,10 @@ class CommunityLink extends Model
     {
         return $this->belongsTo(Channel::class, 'channel_id');
     }
-
-
     public function users()
     {
         return $this->belongsToMany(User::class, 'community_link_users');
-    }
-
+    }   
 
 
     public static function hasAlreadyBeenSubmitted($link)
