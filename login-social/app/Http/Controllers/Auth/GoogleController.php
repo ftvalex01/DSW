bfb0238e6700edd4fc4456b0ad98ef82e09309a4
ftvalex01 <ftvalex@hotmail.com>
2024-01-31 21:12:42 +0000

    ultimos cambios del a28

--- login-social/app/Http/Controllers/Auth/GoogleController.php
+++ login-social/app/Http/Controllers/Auth/GoogleController.php
@@ -2,7 +2,7 @@
   
   namespace App\Http\Controllers\Auth;
   
-  use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Exception;
 use App\Models\User;
